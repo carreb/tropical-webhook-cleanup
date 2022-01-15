08e4219b1d44f819ea9771f83470122e108e0791
carreb <32069843+carreb@users.noreply.github.com>
2022-01-15 00:17:53 -0500

    add formatting to status number

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import asyncio
 import pickle
 import atexit
 from time import sleep
+import configparser
 
 intcounter = 0
 addtocounter = 0
@@ -12,15 +13,15 @@ loop = asyncio.get_event_loop()
 BOT_PREFIX = ">"
 bot = commands.Bot(command_prefix=BOT_PREFIX, case_insensitive=True)
 
-
-with open("bot_token.txt", "r+") as f:
-	token = [i.strip() for i in f.readlines()][0]
+#setup config vars
+config = configparser.ConfigParser()
+config.read('config.ini')
+token = config['info']['token']
+redirectbool = config['info']['redirect-enabled']
+redirectchannel = config['info']['redict-channel']
 with open("counter.txt", "r+") as f:
     counter = [i.strip() for i in f.readlines()][0]
-with open("options.txt", "r+") as f:
-    redirectbool = [i.strip() for i in f.readlines()][0]
-with open("redirect_channel.txt", "r+") as f:
-    redirectchannel = [i.strip() for i in f.readlines()][0]
+
 
 channel = bot.get_channel(int(redirectchannel))
 
@@ -30,8 +31,9 @@ async def update():
     global intcounter
     global counter
     intcounter = int(counter) + addtocounter
-    await bot.change_presence(activity=discord.Game(name=f"{intcounter} invalid pings removed"))
-    print("Updated status! Now at" + str(intcounter))
+    formatcounter = "{:,}".format(intcounter)
+    await bot.change_presence(activity=discord.Game(name=f"{formatcounter} invalid pings removed"))
+    print("Updated status! Now at" + "{:,}".format(intcounter))
     with open("counter.txt", "w+") as f:
         f.write(str(intcounter))
 
